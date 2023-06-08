5f98eb106c316493ef9ed1fce47060eeef6caf69
XDuskAshes <thegaymeradummy@gmail.com>
2023-06-08 04:43:22 -0400

    Kernel: checkbase() and kernelcall()
    When checkbase() is called, it will check the base fs and core files
    included with dawn by default.
    
    kernelcall() is the main global kernel variable, meant for calling
    most if not all functions in kernel.

--- kernel.lua
+++ kernel.lua
@@ -4,7 +4,7 @@
     1.0.0-idev1
 ]]
 
-function _SYSCALL()
+local function _SYSCALL()
     local i = fs.open("/var/.dawninf","r")
     local inf1 = i.readLine(1)
     local inf2 = i.readLine(2)
@@ -17,4 +17,64 @@ function _SYSCALL()
     print(inf4)
 end
 
-_SYSCALL()
+local function checkbase()
+    local basefs = {
+        "/bin/",
+        "/boot/",
+        "/dev/",
+        "/etc/",
+        "/lib/",
+        "/mnt/",
+        "/opt/",
+        "/run/",
+        "/sbin/",
+        "/srv/",
+        "/startup/",
+        "/tmp/",
+        "/var/",
+        "/sys/",
+        "/usr/",
+        "/usr/bin/",
+        "/usr/lib/",
+        "/usr/local/",
+        "/usr/sbin/",
+        "/usr/share/"
+    }
+
+    local core = {
+        "/kernel.lua",
+        "/boot/.bootfile",
+        "/boot/dboot.lua",
+        "/usr/bin/dash.lua"
+    }
+    
+    for _,v in pairs(basefs) do
+        if fs.exists(v) ~= true then
+            printError("/kernel.lua:53:",v,"does not exist.")
+            error()
+        end
+    end
+    
+    for _,v in pairs(core) do
+        if fs.exists(v) ~= true then
+            printError("/kernel.lua:60:",v,"does not exist.")
+            error()
+        end
+    end
+end
+
+--kernelcall()
+
+function kernelcall(func) --call kernel functions
+    local funcs = {
+        [ "_SYSCALL" ] = _SYSCALL,
+        [ "checkbase" ] = checkbase
+    }
+
+    for k,v in pairs(funcs) do
+        if func == k then
+            v()
+            break
+        end
+    end
+end
