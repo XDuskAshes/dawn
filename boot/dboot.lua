aae808f684a0d7a932c7cc45642af351a787199b
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2023-06-08 23:14:53 -0400

    use `fs` calls rather than `shell.run` where reasonable

--- boot/dboot.lua
+++ boot/dboot.lua
@@ -1,44 +1,43 @@
 --dawn default boot program
 --by dusk
 
+local function setboot()
+    write("Boot file path:")
+    local bootfileNew = read()
+    print("remove bootfile")
+    fs.delete("/boot/.bootfile")
+    print("copy from /etc/backup/")
+    fs.copy("/etc/backup/.bootfile", "/tmp/.bootfile")
+    print("open and write",bootfileNew,"to /tmp/.bootfile")
+    local tmpb = fs.open("/tmp/.bootfile","w")
+    tmpb.write(bootfileNew)
+    tmpb.close()
+    print("move to /boot/")
+    fs.move("/tmp/.bootfile", "/boot/.bootfile")
+end
+
+local function setbail()
+    write("bail file path:")
+    local bailfileNew = read()
+    print("remove bailto")
+    fs.delete("/boot/.bailto")
+    print("copy from /etc/backup/")
+    fs.copy("/etc/backup/.bailto", "/tmp/.bailto")
+    print("open and write",bailfileNew,"to /tmp/.bailto")
+    local tmpb = fs.open("/tmp/.bailto","w")
+    tmpb.write(bailfileNew)
+    tmpb.close()
+    print("move to /boot/")
+    fs.move("/tmp/.bailto" "/boot/.bailto")
+end
+
+local limitshell = {
+    [ "setboot" ] = setboot,
+    [ "setbail" ] = setbail,
+    [ "reboot" ] = os.reboot
+}
+
 local function run(a)
-
-    local function setboot()
-        write("Boot file path:")
-        local bootfileNew = read()
-        print("remove bootfile")
-        shell.run("rm /boot/.bootfile")
-        print("copy from /etc/backup/")
-        shell.run("copy /etc/backup/.bootfile /tmp/.bootfile")
-        print("open and write",bootfileNew,"to /tmp/.bootfile")
-        local tmpb = fs.open("/tmp/.bootfile","w")
-        tmpb.write(bootfileNew)
-        tmpb.close()
-        print("move to /boot/")
-        shell.run("move /tmp/.bootfile /boot/")
-    end
-
-    local function setbail()
-        write("bail file path:")
-        local bailfileNew = read()
-        print("remove bailto")
-        shell.run("rm /boot/.bailto")
-        print("copy from /etc/backup/")
-        shell.run("copy /etc/backup/.bailto /tmp/.bailto")
-        print("open and write",bailfileNew,"to /tmp/.bailto")
-        local tmpb = fs.open("/tmp/.bailto","w")
-        tmpb.write(bailfileNew)
-        tmpb.close()
-        print("move to /boot/")
-        shell.run("move /tmp/.bailto /boot/")
-    end
-
-    local limitshell = {
-        [ "setboot" ] = setboot,
-        [ "setbail" ] = setbail,
-        [ "reboot" ] = os.reboot
-    }
-
     for k,v in pairs(limitshell) do
         if a == k then
             v()
