5254bd88f75bc04bcf624917889892abb747c949
XDuskAshesReal <101196233+XDuskAshes@users.noreply.github.com>
2023-06-09 14:10:38 -0400

    Update dash.lua
    Again no idea if this works atm

--- usr/bin/dash.lua
+++ usr/bin/dash.lua
@@ -9,3 +9,15 @@ local root = fs.list("/")
 local ver = fs.open("/var/.dawninf")
 local version = ver.readLine(1)
 ver.close()
+local target
+
+local function targetdir()
+    write("target is:")
+    local t = read()
+    target == t
+end
+
+local cmd = {
+    [ "targetdir" ] = targetdir
+}
+
