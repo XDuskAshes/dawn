3e02743061ab2ac9716084448413520214370a7a
XDuskAshes <thegaymeradummy@gmail.com>
2023-06-16 17:01:31 -0400

    mhm

---
+++ dash/fetch.lua
@@ -0,0 +1,13 @@
+--[[
+    dawn fetch
+    by dusk
+]]
+
+local ver = fs.open("/stat/.dawninf", "r")
+local z = ver.readLine(1)
+local vers = ver.readLine(2)
+ver.close()
+
+print("runtime",_HOST)
+print("lua",_VERSION)
+print(vers)
