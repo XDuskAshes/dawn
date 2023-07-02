f329490ff4464aab85366eda73c71e02a838a9da
XDuskAshes <thegaymeradummy@gmail.com>
2023-07-02 13:14:40 -0400

    start redoing per

--- bin/per.lua
+++ bin/per.lua
@@ -11,115 +11,14 @@ local per = peripheral.getNames()
 
 if k.empty(args[1]) then
     print("Usage: read the docs")
-elseif args[1] == "-m" then
+    error()
+end
 
-    if not periphemu then k.scrMSG(5,"-m only available in CCPC") end
-
-    if k.empty(args[2]) then
-        print("No side given")
+if args[1] == "-m" then
+    if not periphemu then
+        print("-m only available in CCPC")
         error()
-    elseif k.empty(args[3]) then
-        print("No type given")
-        error()
-    elseif k.isSide(args[2]) ~= true then
-        k.scrMSG(5,"Not a side:"..args[2])
-    end
-
-    if args[3] == "disk" then
-        if k.empty(args[4]) then
-            k.scrMSG(5,"No mount path given.")
-        else
-            periphemu.create(args[2], args[3], args[4])
-        end
     else
-        periphemu.create(args[2], args[3])
-    end
-
-
-elseif args[1] == "-l" then
-    per = peripheral.getNames()
-    for i,v in ipairs(per) do
-        local b = peripheral.getType(v)
-        print(v,b)
-    end
-elseif args[1] == "disk" then
-    if k.empty(args[2]) then
-        print("Read docs for usage of",args[1])
-    end
-    elseif args[2] == "-l" then
-        local drive = peripheral.find("drive") or error("no drives")
-        print("| SIDE | MOUNT |")
-        for i,v in ipairs(per) do
-            local t = peripheral.getType(v)
-            if t == "drive" then
-                if k.empty(args[3]) then
-                    local audio = disk.hasAudio(v)
-                    local present = disk.isPresent(v)
-                        if present then
-                            if audio then
-                                mnt = "N/A"
-                            else
-                                mnt = disk.getMountPath(v)
-                            end
-                            write("["..v.."]|"..mnt.."|\n")
-                        else
-                            write("["..v.."]|nil|\n")
-                        end
-                    elseif args[3] == "-v" then
-                        local audio = disk.hasAudio(v)
-                        local present = disk.isPresent(v)
-                        if present then
-                            if audio then
-                                mnt = "N/A"
-                            else
-                                mnt = disk.getMountPath(v)
-                            end
-                            local cap = fs.getCapacity(mnt)
-                            local free = fs.getFreeSpace(mnt)
-                            write("+["..v.."]|"..mnt.."|\n")
-                            write("|Capacity: "..cap.."|Space Free: "..free.."|\n")
-                        else
-                            write("["..v.."]|nil|\n")
-                        end
-                end
-            end
-        end
-    elseif args[2] == "-e" then
-        if k.empty(args[2]) then
-            print("Usage: disk -e <side>")
-        else
-            for _,v in ipairs(per) do
-                local present = disk.isPresent(v)
-                if present then
-                    disk.eject(args[3])
-                end
-            end
-        end
-    elseif args[2] == "-p" then
-        if k.empty(args[3]) then
-            print("Usage: per disk -p <file/path>")
-        else
-            for i,v in ipairs(per) do
-                if k.isSide(v) ~= true then
-                    local mont = disk.getMountPath(v)
-                    if mont ~= nil then
-                        if fs.exists(mont.."/"..args[3]) then
-                            print(args[3],"present on",mont,"("..v..")")
-                        end
-                    else
-                        print("Encountered no disk mount on",v)
-                    end
-                end
-            end
-        end
-    else
-        print("Supplied argument '"..args[2].."' is not applicable.")
-    end
-
-if args[1] == "modem" then
-    if k.empty(args[2]) then
-        print("Read docs for usage of",args[1])
-    elseif args[2] == "-l" then
-
+        print("Work on this in ccpc")
     end
 end
